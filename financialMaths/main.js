8d693e4cdb10e954fb72aa29a700b0e097480f38
Selekedi <106023979+Selekedi@users.noreply.github.com>
2025-05-20 09:08:55 +0200

    Update main.js balance after k years fix

--- financialMaths/main.js
+++ financialMaths/main.js
@@ -261,7 +261,7 @@ function populateForm(category,method){
                     inputs.appendChild(submitButton)
                 break
                 case "balance after k years":
-                    inputs.append(presentValue,payments,periodsPerYear,Kyears)
+                    inputs.append(presentValue,payments,rate,periodsPerYear,Kyears)
                     submitButton.onclick = () => {
                         const PV= getValue(presentValue)
                         const PM = getValue(payments)
